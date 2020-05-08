59f44607ddd11183b17956cb24628d180db9fd59
CEO of racism <ernestsbush12@gmail.com>
2020-05-08 16:48:58 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<!--
+	Transitive by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>WASD computers</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">WASD <span>COMPUTERS</span></a></div>
+				<a href="#menu" class="toggle"><span>Menu</span></a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+		<!--
+			To use a video as your background, set data-video to the name of your video without
+			its extension (eg. images/banner). Your video must be available in both .mp4 and .webm
+			formats to work correctly.
+		-->
+			<section id="banner" data-video="images/banner">
+				<div class="inner">
+					<h1>WASD COMPUTERS</h1>
+					<p>You choose, we build it!<br />
+					<a href="#one" class="button special scrolly">Get Started</a>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<div>
+						<div class="box">
+							
+							</div>
+							<div class="content">
+								<header class="align-center">
+									<h2>Sup bitches</h2>
+									<p>yolo</p>
+								</header>
+								<hr />
+								<p> The flora of this world is much like the real world's: diverse with many biomes, including swamps, forests, deserts, mountains, and snowy environments, alongside with rivers and oceans. The only known intelligent and talking woods are the Evil Trees and Pinocchio. Fungi can be affected by magic, as seen in Shrek 2 when Shrek sneezed the Happily Ever After Potion at a mushroom and it transformed into a rose minutes later.</p>
+								<p>The creatures inhabiting this world are very diverse. Humans, ogres and cats are intelligent and able to speak. The same may be said about most other species, although some only from what is known of individual examples. Dragon, the only known dragon, clearly understands what humans say, but does not speak. Other species, like horses, dogs and birds, are seemly not intelligent at all, just like their real-life counterparts. Some food also have life, like Gingerbread Man (a gingerbread cookie) and Humpty Dumpty (an egg).</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style3">
+				<div class="inner">
+					<div id="flexgrid">
+						<div>
+							<header>
+								<h3>Pre-builts</h3>
+							</header>
+							<p>Get yourself a complete pre-built system.</p>
+							<ul class="actions">
+								<li><a href="#" class="button alt">Learn More</a></li>
+							</ul>
+						</div>
+						<div>
+							<header>
+								<h3>Custom</h3>
+							</header>
+							<p>Pick you own components </p>
+							<ul class="actions">
+								<li><a href="#" class="button alt">Learn More</a></li>
+							</ul>
+						</div>
+						<div>
+							<header>
+								<h3>Shrek dvds</h3>
+							</header>
+							<p>Get your favourite dvd's</p>
+							<ul class="actions">
+								<li><a href="#" class="button alt">Learn More</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div>
+							<div class="box">
+								<div class="image fit">
+									<img src="images/pic02.jpg" alt="" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<h2>Pre-built systems</h2>
+										<p>Best price and performance</p>
+									</header>
+									<hr />
+									<p> We use only the best components from reliable and trusted vendors. These systems are carefully picked and built for maximum performance, reliabilty and price.</p>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit">
+									<img src="images/pic03.jpg" alt="" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<h2>Custom built systems</h2>
+										<p>Make it the way u want it.</p>
+									</header>
+									<hr />
+									<p> We provide an easy to use system for you to select your system and we will put it all together.</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style3">
+				<div class="inner">
+
+					<header class="align-center">
+						<h2>Why choose our services?</h2>
+						<p> We the best.</p>
+					</header>
+
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper">
+				<div class="inner">
+					<section>
+						<div class="box">
+							<div class="content">
+								<h2 class="align-center">Get in Touch</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field half first">
+										<label for="name">Name</label>
+										<input name="name" id="name" type="text" placeholder="Name">
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input name="email" id="email" type="email" placeholder="Email">
+									</div>
+									<div class="field">
+										<label for="dept">Department</label>
+										<div class="select-wrapper">
+											<select name="dept" id="dept">
+												<option value="">- Category -</option>
+												<option value="1">Manufacturing</option>
+												<option value="1">Shipping</option>
+												<option value="1">Administration</option>
+												<option value="1">Human Resources</option>
+											</select>
+										</div>
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions align-center">
+										<li><input value="Send Message" class="button special" type="submit"></li>
+									</ul>
+								</form>
+							</div>
+						</div>
+					</section>
+					<div class="copyright">
+						&copy; Untitled Design: <a href="https://templated.co/">TEMPLATED</a>. Images <a href="https://unsplash.com/">Unsplash</a>. Video <a href="http://coverr.co/">Coverr</a>.
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
